b27875510a46f8790a256f1c02a28a7b0a121c28
Joseph <joseph.pena@utec.edu.pe>
2021-06-12 18:04:56 -0300

    Add initial version with decimal numbers

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+unsigned
+applyOperator(std::string op, unsigned v1, unsigned v2) {
+	if (op == "+") {
+		return v1 + v2;
+	} else {
+		throw "Invalid operator";
+	}
+}
+
+unsigned
+getNumber(std::string v) {
+	std::string numberSystem = v.substr(0, 2);
+	std::string number = v.substr(2, v.length() - 2);
+	if (numberSystem == "0d" || numberSystem == "0D") {
+		return std::stoi(number);
+	} else {
+		throw "Invalid number system";
+	}
+}
+
+unsigned
+getResult(std::string line) {
+	std::stringstream ss(line);
+	std::string v1, op, v2;
+	ss >> v1;
+	ss >> op;
+	ss >> v2;
+	unsigned vv1 = getNumber(v1);
+	unsigned vv2 = getNumber(v2);
+	return applyOperator(op, vv1, vv2);
+}
+
+void
+printResult(unsigned number) {
+	std::cout << "0d" << number << std::endl;
+}
+
+int
+main() {
+	std::cout << "=================================" << std::endl;
+	std::cout << "Welcome to the Awesome Calculator" << std::endl;
+	std::cout << "=================================" << std::endl;
+
+	std::string line;
+	while (getline(std::cin, line)) {
+		printResult(getResult(line));
+	}
+	return 0;
+}
