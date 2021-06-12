ef1996a7a91926cdb43f257307f45f2e329d9e18
Joseph <joseph.pena@utec.edu.pe>
2021-06-12 18:54:04 -0300

    Add binary operators | and &

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@ applyOperator(std::string op, unsigned v1, unsigned v2) {
 		return v1 + v2;
 	if (op == "-")
 		return v1 - v2;
+	if (op == "|")
+		return v1 | v2;
+	if (op == "&")
+		return v1 & v2;
 	else
 		throw "Invalid operator";
 }
